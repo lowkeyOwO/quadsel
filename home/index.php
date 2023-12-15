e8c7fd9c8078cdcccfc64b87f8bec379116bc5cb
lowkeyOwO <lokesh.varathan1593@gmail.com>
2023-12-15 12:52:26 +0530

    Day 03: Testing charts and admin page planning

--- home/index.php
+++ home/index.php
@@ -1,11 +1,38 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Homepage</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Chart.js Dummy Code</title>
+  <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 </head>
 <body>
-    welcome home 👍
+<canvas id="myChart" width="400" height="200"></canvas>
+<script>
+var data = {
+  labels: ['January', 'February', 'March', 'April', 'May'],
+  datasets: [{
+    label: 'Dummy Bar Chart',
+    backgroundColor: 'rgba(75, 192, 192, 0.2)',
+    borderColor: 'rgba(75, 192, 192, 1)',
+    borderWidth: 1,
+    data: [65, 59, 80, 81, 56],
+  }]
+};
+var options = {
+  scales: {
+    y: {
+      beginAtZero: true
+    }
+  }
+};
+var ctx = document.getElementById('myChart').getContext('2d');
+var myChart = new Chart(ctx, {
+  type: 'bar',
+  data: data,
+  options: options
+});
+</script>
+
 </body>
 </html>
